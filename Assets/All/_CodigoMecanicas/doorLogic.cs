149efcf7ad930cc8531252d2fc56558cb790a0f3
blak352yt <72416920+blak352yt@users.noreply.github.com>
2022-05-05 21:51:03 -0500

    Codigo final
    Codigo final

--- Assets/All/_CodigoMecanicas/doorLogic.cs
+++ Assets/All/_CodigoMecanicas/doorLogic.cs
@@ -1,93 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class doorLogic : MonoBehaviour
 {
-	public GameObject puerta;
-	public GameObject palanca;
-	//abrir puerta
-	public float speed;
-	public float distance;
+    public GameObject puerta;
+    public GameObject palanca;
+    //abrir puerta
+    public float speed;
+    public float distance;
 
-	//Cerrar la puerta
-	public float distanceCerrado;
-	public Vector3 rotationCerrado;
+    //Cerrar la puerta
+    public float distanceCerrado;
+    public Vector3 rotationCerrado;
 
-	public Vector3 rotation;
-	public float speedlevel;
+    public Vector3 rotation;
+    public float speedlevel;
 
-	public bool puertaAbierta;
+    public bool puertaAbierta;
+
+    public GameObject player;
 
     bool Cerca;
 
 
-    
 
 
-	private void OnTriggerStay(Collider other)
-	{
-		listoparausar();
 
-<<<<<<< HEAD
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (Keyboard.current.xKey.wasPressedThisFrame && puertaAbierta == false)
+            {
+                abrirpuerta();
+            }
+            if (Keyboard.current.xKey.wasPressedThisFrame && puertaAbierta == true)
+            {
+                cerrarlapuerta();
+            }
+
         }
         Cerca = true;
     }
-=======
-
-	}
-
-
-	public void listoparausar()
-	{
-		if (Keyboard.current.eKey.wasPressedThisFrame && puertaAbierta == false)
-		{
-			Debug.Log("Abrir puerta ");
-			puerta.transform.DOLocalMoveY(distance, speed);
-			palanca.transform.DOLocalRotate(rotation, speedlevel);
-			puertaAbierta = true;
-
-
-
-		}
-		else
-		{
-			//Cerrar la puerta
-			if (Keyboard.current.eKey.wasPressedThisFrame && puertaAbierta == true)
-			{
-				Debug.Log("cerrar puerta ");
-				puerta.transform.DOLocalMoveY(distanceCerrado, speed);
-				palanca.transform.DOLocalRotate(rotationCerrado, speedlevel);
-				puertaAbierta = false;
-
-			}
-		}
->>>>>>> 850361b851969d086bbce8d96e1bfadfd36b558c
-
-	private void OnTriggerExit(Collider other)
-	{
-        Cerca = false;
-	}
-
-
-
-<<<<<<< HEAD
-	public void abrirpuerta()
+    private void OnTriggerExit(Collider other)
     {
-=======
-		}
->>>>>>> 850361b851969d086bbce8d96e1bfadfd36b558c
-
-	public void insusable() { 
-	}
+        Cerca = false;
+    }
 
 
 
-   public void UIclick()
-	{
+    public void abrirpuerta()
+    {
+
+        Debug.Log("Abrir puerta ");
+        puerta.transform.DOLocalMoveY(distance, speed);
+        palanca.transform.DOLocalRotate(rotation, speedlevel);
+
+        StartCoroutine(CoroutineAbierta());
+
+
+    }
+
+    public void cerrarlapuerta()
+    {
+        Debug.Log("cerrar puerta ");
+        puerta.transform.DOLocalMoveY(distanceCerrado, speed);
+        palanca.transform.DOLocalRotate(rotationCerrado, speedlevel);
+        StartCoroutine(CoroutineCerrar());
+
+    }
+
+    IEnumerator CoroutineAbierta()
+    {
+        //espera 0.5 segundos
+        yield return new WaitForSeconds(1f);
+        puertaAbierta = true;
+    }
+
+    IEnumerator CoroutineCerrar()
+    {
+        //espera 0.5 segundos
+        yield return new WaitForSeconds(1f);
+        puertaAbierta = false;
+    }
+
+    public void UIclick()
+    {
         if (puertaAbierta == false && Cerca == true)
         {
             abrirpuerta();
@@ -100,10 +103,7 @@ public class doorLogic : MonoBehaviour
             }
         }
     }
+
+
 }
 
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 850361b851969d086bbce8d96e1bfadfd36b558c
